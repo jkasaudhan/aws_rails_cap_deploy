886f18b2562e8edec2f2708f64ebe4b6159173a7
Jitendra Kasaudhan <jitendra.kasaudhan@wellsterhealth.com>
2021-03-19 15:57:40 +0100

    Used rbenv 2.5.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,8 @@ set :repo_url, 'https://github.com/jkasaudhan/aws_rails_cap_deploy.git'
 set :deploy_to, '/var/www/aws_rails_cap_deploy'
 set :branch, ENV['BRANCH'] if ENV['BRANCH']
 
+set :rbenv_ruby, 'ruby2.5.1'
+
 set :linked_files, %w{config/database.yml config/master.key}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
